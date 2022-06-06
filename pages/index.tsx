bd12f88a83b9eaaa293da36b10b3f4d966c19755
Max Baravy <maximkatut@gmail.com>
2022-06-06 10:46:37 -0400

    fix styling and move font family from link tag to css import

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,7 @@ const Home: NextPage = () => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={title} />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet"/> 
+        <link rel="icon" href="/favicon.ico" /> 
       </Head>
 
       <main className={styles.main}>
