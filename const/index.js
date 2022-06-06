7e737d5ebceec8c5cfe159107ab09ee3f21a74f3
Max Baravy <maximkatut@gmail.com>
2022-06-06 13:38:29 -0400

    initial state for first load

---
+++ const/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  ip: "1.1.1.1",
+  location: {
+    region: "CA",
+    city: "Los Angeles",
+    lat: 34.05223,
+    lng: -118.24368,
+    postalCode: "90001",
+    timezone: "-7.00",
+  },
+  isp: "APNIC Research and Development",
+};
